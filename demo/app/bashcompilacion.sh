63ce1c011496caa1a2a64ed6a2d0ae1523a103f4
guevaralb@gmail.com <guevaralb@gmail.com>
2020-10-09 10:27:12 -0500

    con ejecutable compilacion y arregladas direcciones

--- demo/app/bashcompilacion.sh
+++ demo/app/bashcompilacion.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 g++ main.cpp p2_client.cpp /data/Dash/demo/base/common_utils.cpp /data/Dash/demo/base/json_utils.cpp /data/Dash/demo/base/log.cpp /data/Dash/demo/base/md5.cpp /data/Dash/demo/net/channel.cpp /data/Dash/demo/net/poller.cpp  /data/Dash/demo/net/socket_utils.cpp /data/Dash/demo/json/json_reader.cpp /data/Dash/demo/json/json_value.cpp /data/Dash/demo/json/json_writer.cpp  -o jdemo
-./jdemo
+
